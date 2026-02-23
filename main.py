1600264c0030f572f5373243958544af23b59d27
HICHAMBOUMZGOU123 <160008051+HICHAMBOUMZGOU123@users.noreply.github.com>
2026-02-23 12:33:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,81 +3,86 @@ import os
 import openpyxl
 from operator import itemgetter
 
-from traitement_excel import *
+from traitement_excel import (
+    folderChoice,
+    folderconvert,
+    filetest,
+    checkfile,
+    takeclientlist,
+    getcustomersdata
+)
 
 
-currentfolderwin = str(folderChoice())
-currentfolder = str(folderconvert(currentfolderwin))
+def main():
+    currentfolderwin = str(folderChoice())
+    currentfolder = str(folderconvert(currentfolderwin))
 
-fileInFolderList = (listdir(currentfolder))
+    fileInFolderList = listdir(currentfolder)
 
-filelist = filetest(fileInFolderList)
-print("les fichiers retenu sont " + str(filelist))
+    filelist = filetest(fileInFolderList)
+    print("les fichiers retenu sont " + str(filelist))
 
-os.chdir(currentfolderwin)
-print(currentfolder)
+    os.chdir(currentfolderwin)
 
-checkfile(filelist)
+    checkfile(filelist)
 
-allCustomersInFolderList = takeclientlist(filelist)
-print(allCustomersInFolderList)
+    allCustomersInFolderList = takeclientlist(filelist)
 
-customersdatalist = getcustomersdata(allCustomersInFolderList, filelist)
-print(customersdatalist)
+    customersdatalist = getcustomersdata(allCustomersInFolderList, filelist)
 
-for check in customersdatalist:
-    for x in check:
-        i = 0
-        for checkin in x:
-            if checkin == None:
-                x[i] = 0
-            i += 1
+    # Nettoyage des valeurs None
+    for check in customersdatalist:
+        for x in check:
+            for i in range(len(x)):
+                if x[i] is None:
+                    x[i] = 0
 
-print('initialisation terminer')
+    # Tri par date
+    for toto in customersdatalist:
+        toto.sort(key=itemgetter(1))
 
-for toto in customersdatalist:
-    get_n = itemgetter(1)
-    myList = toto
-    myList.sort(key=get_n)
+    # Création du dossier factureclients
+    output_folder = os.path.join(currentfolder, "factureclients")
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
-if not os.path.exists(str(currentfolder) + "/factureclients"):
-    os.makedirs(str(currentfolder) + "/factureclients")
+    os.chdir(output_folder)
 
-os.chdir(currentfolderwin + "/factureclients")
+    # Génération des factures
+    for data in customersdatalist:
+        wbOutput = openpyxl.Workbook()
+        sheet = wbOutput.active
 
-for data in customersdatalist:
-    wbOutput = openpyxl.Workbook()
-    sheet = wbOutput.active
-    sheet["A1"] = "Facture"
-    sheet["B6"] = "DATE"
-    sheet["C6"] = "Maxi"
-    sheet["D6"] = "Geant"
-    sheet["E6"] = "Miche"
-    sheet["F6"] = "Galette"
-    sheet["G6"] = "Somun"
-    sheet["H6"] = "Marguerite"
-    sheet["I6"] = "Pide"
+        sheet["A1"] = "Facture"
+        sheet["B6"] = "DATE"
+        sheet["C6"] = "Maxi"
+        sheet["D6"] = "Geant"
+        sheet["E6"] = "Miche"
+        sheet["F6"] = "Galette"
+        sheet["G6"] = "Somun"
+        sheet["H6"] = "Marguerite"
+        sheet["I6"] = "Pide"
 
-    linetable = 7
-    for inDATA in data:
-        filenameOutput = str(inDATA[0])
-        date = inDATA[1].strftime("%d/%m/%Y")
-        maxi = inDATA[2]
-        geant = inDATA[3]
-        miche = inDATA[4]
-        galette = inDATA[5]
-        somun = inDATA[6]
-        marguerite = inDATA[7]
-        pide = inDATA[8]
-        sheet["A2"] = str(filenameOutput)
-        sheet["B" + str(linetable)] = str(date)
-        sheet["C" + str(linetable)] = int(maxi)
-        sheet["D" + str(linetable)] = int(geant)
-        sheet["E" + str(linetable)] = int(miche)
-        sheet["F" + str(linetable)] = int(galette)
-        sheet["G" + str(linetable)] = int(somun)
-        sheet["H" + str(linetable)] = int(marguerite)
-        sheet["I" + str(linetable)] = int(pide)
-        linetable += 1
+        linetable = 7
 
-    wbOutput.save(str(filenameOutput + ".xlsx"))
+        for inDATA in data:
+            filenameOutput = str(inDATA[0])
+            date = inDATA[1].strftime("%d/%m/%Y")
+
+            sheet["A2"] = filenameOutput
+            sheet["B" + str(linetable)] = date
+            sheet["C" + str(linetable)] = int(inDATA[2])
+            sheet["D" + str(linetable)] = int(inDATA[3])
+            sheet["E" + str(linetable)] = int(inDATA[4])
+            sheet["F" + str(linetable)] = int(inDATA[5])
+            sheet["G" + str(linetable)] = int(inDATA[6])
+            sheet["H" + str(linetable)] = int(inDATA[7])
+            sheet["I" + str(linetable)] = int(inDATA[8])
+
+            linetable += 1
+
+        wbOutput.save(filenameOutput + ".xlsx")
+
+
+if __name__ == "__main__":
+    main()
